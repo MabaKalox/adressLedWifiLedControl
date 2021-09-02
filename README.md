257b9e70599d438f6a3387ac404311c614039ea8
maba_kalox <llocnex@gmail.com>
2021-09-03 01:37:41 +0300

    Implement save led strip state option

--- README.md
+++ README.md
@@ -20,16 +20,24 @@
 GET - `/`
 Response: `200, Hello, world`
 
+GET - `/remove_saved_led_strip_state`
+Response: `200, OK`
+
 POST - `/set_led_strip`
 
 * Request body: `multipart form`.
     * `color_array` - list of hex encoded `W` `R` `G` `B`, e.g: `000f000f,000f0000`
+    * `save` - save or not settings: `1`/`0`
 * Response: `200, APPLIED`
 
 ## Pin out
 
 1. D2 – led strip data pin throught 50 ohm resistor.
 2. D5 – Wi-Fi credentials reset button pin.
-3. VIN – power pin thorough [1n4007](https://www.vishay.com/docs/88503/1n4001.pdf) diode.
+
+## Suggestions
+1. Add diode ([`1n4007`](https://www.vishay.com/docs/88503/1n4001.pdf) or similar) on VIN Pin, to prevent power of led strip from microcontroller side.
+2. Add `1kΩ` resistor to D2 pin as current limiting resistor.
+3. Add Decoupling Capacitors `400uF+` and `5V+` between gnd and VIN pin of microcontroller.
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 #include <GParser.h>
-#include <array>
 #include <cstring>
 #include <string>
 #include <ESPAsyncWebServer.h>
@@ -10,9 +9,12 @@
 #include <ESP8266mDNS.h>
 #include <EEPROM.h>
 
-#define COLOR_ARRAY_START_ADDRESS 0
+#define COLOR_ARRAY_FLAG_ADDRESS 0
+#define COLOR_ARRAY_START_ADDRESS 1
 
 #define PIXEL_QUANTITY 150
+
+#define EEPROM_SIZE PIXEL_QUANTITY * sizeof(uint32_t) + 1
 
 #define HOSTNAME "led_strip"
 
@@ -24,13 +26,21 @@ AsyncWebServer server(80);
 
 EncButton<EB_CALLBACK, D5> wifi_reset_button;
 
-std::array<uint32_t, PIXEL_QUANTITY> read_color_array_from_eeprom() {
-    std::array<uint32_t, PIXEL_QUANTITY> color_array{0};
-    return color_array;
+template<class ContentType, int const ContentLength>
+void read_array_eeprom(int address, ContentType *to_read_into) {
+    for (int i = 0; i < ContentLength; i++) {
+        EEPROM.get(address, to_read_into[i]);
+        address += sizeof(ContentType);
+    }
 }
 
-void save_color_array_to_eeprom(std::array<uint32_t, PIXEL_QUANTITY> &target) {
-
+template<class ContentType, int const ContentLength>
+void save_array_eeprom(int address, const ContentType *to_save) {
+    for (int i = 0; i < ContentLength; i++) {
+        EEPROM.put(address, to_save[i]);
+        address += sizeof(ContentType);
+    }
+    EEPROM.commit();
 }
 
 void applyColorArray(uint32_t *target) {
@@ -66,6 +76,12 @@ void set_up_server() {
         request->send(200, "text/plain", "Hello, world");
     });
 
+    server.on("/remove_saved_led_strip_state", HTTP_GET, [](AsyncWebServerRequest *request) {
+        EEPROM.put(COLOR_ARRAY_FLAG_ADDRESS, false);
+        EEPROM.commit();
+        request->send(200, "text/plain", "OK");
+    });
+
     server.on("/set_led_strip", HTTP_POST, [](AsyncWebServerRequest *request) {
         if (request->hasParam("color_array", true)) {
             String received_data = request->getParam("color_array", true)->value();
@@ -79,6 +95,12 @@ void set_up_server() {
                     color_array[i] = std::stoul(received_data_parsed[i], nullptr, 16);
                 }
                 applyColorArray(color_array);
+                if (request->hasParam("save", true) && *(request->getParam("save", true)->value().c_str()) == '1') {
+                    save_array_eeprom<uint32_t, PIXEL_QUANTITY>(COLOR_ARRAY_START_ADDRESS, color_array);
+                    EEPROM.put(COLOR_ARRAY_FLAG_ADDRESS, true);
+                    EEPROM.commit();
+                    Serial.println("Saving");
+                }
                 request->send(200, "text/plain", "APPLIED");
             } else {
                 request->send(
@@ -115,12 +137,25 @@ void reset_wifi() {
     Serial.println("Resetting WM data.");
 }
 
+void apply_last_setting_if_exists() {
+    bool is_set = false;
+    EEPROM.get(COLOR_ARRAY_FLAG_ADDRESS, is_set);
+    if (is_set) {
+        uint32_t color_array[PIXEL_QUANTITY]{0};
+        read_array_eeprom<uint32_t, PIXEL_QUANTITY>(COLOR_ARRAY_START_ADDRESS, color_array);
+        applyColorArray(color_array);
+    }
+}
+
 void setup() {
     Serial.begin(115200);
+    EEPROM.begin(EEPROM_SIZE);
 
     strip.begin();
     strip.setBrightness(255);
     strip.show();
+
+    apply_last_setting_if_exists();
 
     wifi_reset_button.attach(CLICKS_HANDLER, reset_wifi);
     wifi_reset_button.pullUp();
@@ -138,15 +173,38 @@ uint32_t last_heap_check_time = 0;
 void loop() {
     wifi_reset_button.tick();
 
-    if (millis() - last_update > 30000) {
-        Serial.println("update mdns");
+    if (millis() - last_update > 2000) {
+//        Serial.println("update mdns");
         MDNS.update();
         last_update = millis();
     }
 
-    if (millis() - last_heap_check_time > 500) {
-        last_heap_check_time = millis();
-        Serial.print("Free Heap: ");
-        Serial.println(ESP.getFreeHeap());
+//    if (millis() - last_heap_check_time > 500) {
+//        last_heap_check_time = millis();
+//        Serial.print("Free Heap: ");
+//        Serial.println(ESP.getFreeHeap());
+//        Serial.print("Wifi status: ");
+//        Serial.println(WiFi.isConnected());
+//    }
+
+    if (Serial.available()) {
+//        EEPROM.put(COLOR_ARRAY_FLAG_ADDRESS, 0);
+//        EEPROM.commit();
+        bool is_set = false;
+        EEPROM.get(COLOR_ARRAY_FLAG_ADDRESS, is_set);
+        if (is_set) {
+            uint32_t buff[PIXEL_QUANTITY]{0};
+            read_array_eeprom<uint32_t, PIXEL_QUANTITY>(COLOR_ARRAY_START_ADDRESS, buff);
+            for (unsigned int v : buff) {
+                Serial.print(v);
+                Serial.print(",");
+            }
+            Serial.println();
+        } else {
+            Serial.println("Array not set");
+        }
+        while (Serial.available()) {
+            Serial.read();
+        }
     }
 }
